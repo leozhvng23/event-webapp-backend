08633353c99b85d17c2990406993b3c2c20ea0e9
leozhvng23 <57277493+leozhvng23@users.noreply.github.com>
2023-04-25 11:59:47 -0400

    cors headers

--- LF1/index.mjs
+++ LF1/index.mjs
@@ -5,6 +5,11 @@ const region = "us-east-1";
 const ddbClient = new DynamoDBClient({ region });
 const ddbDocClient = DynamoDBDocumentClient.from(ddbClient);
 
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Credentials": true,
+};
+
 const handler = async (event) => {
   console.log("Received event:", JSON.stringify(event, null, 2));
   const { id } = event.pathParameters;
@@ -12,46 +17,58 @@ const handler = async (event) => {
   if (!id || typeof id !== "string") {
     const response = {
       statusCode: 400,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
+      headers: corsHeaders,
       body: JSON.stringify({ message: "Invalid input data." }),
     };
 
     return response;
   }
 
-  const params = {
-    TableName: "Eventful-Events",
-    Key: {
-      id: id,
-    },
-  };
-
   try {
-    const data = await ddbDocClient.send(new GetCommand(params));
+    const eventParams = {
+      TableName: "Eventful-Events",
+      Key: {
+        id: id,
+      },
+    };
 
-    if (!data.Item) {
+    const eventData = await ddbDocClient.send(new GetCommand(eventParams));
+
+    if (!eventData.Item) {
       const response = {
         statusCode: 404,
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Credentials": true,
-        },
+        headers: corsHeaders,
         body: JSON.stringify({ message: "Event not found." }),
       };
 
       return response;
     }
 
+    const userParams = {
+      TableName: "Eventful-Users",
+      Key: {
+        id: eventData.Item.uid,
+      },
+    };
+
+    const userData = await ddbDocClient.send(new GetCommand(userParams));
+
+    if (!userData.Item) {
+      const response = {
+        statusCode: 404,
+        headers: corsHeaders,
+        body: JSON.stringify({ message: "Host not found." }),
+      };
+      return response;
+    }
+
+    // add host name to event data
+    eventData.Item.hostName = userData.Item.name;
+
     const response = {
       statusCode: 200,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
-      body: JSON.stringify(data.Item),
+      headers: corsHeaders,
+      body: JSON.stringify(eventData.Item),
     };
 
     return response;
@@ -60,10 +77,7 @@ const handler = async (event) => {
 
     const response = {
       statusCode: 500,
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Credentials": true,
-      },
+      headers: corsHeaders,
       body: JSON.stringify({ message: "Error fetching event." }),
     };
 
